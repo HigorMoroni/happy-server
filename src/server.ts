e8c0bf5b92bebbc88fda9fcabc6169a70d10f62e
Higor Moroni <higor.moroni@prodigious.com>
2021-01-11 15:26:37 -0300

    Recebendo dados e criando uma tupla

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,38 @@
 import express from 'express';
 import "reflect-metadata";
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanages';
 
 import './database/connection';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.post('/users', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage); 
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json(orphanage);
 });
 
 app.listen(3333);
