4b1b0d35dd68fffadbe1a826a47294e10b8495fc
Higor Moroni <higor.moroni@prodigious.com>
2021-01-14 16:06:19 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import 'express-async-errors';
 import "reflect-metadata";
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 import errorHandler from './errors/handler';
 
 import './database/connection';
 
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
