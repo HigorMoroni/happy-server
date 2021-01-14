3a91a70f6b6d81d867bc6ae4fab0c8d1ea4846e5
Higor Moroni <higor.moroni@prodigious.com>
2021-01-14 14:28:25 -0300

    Tratando erros

---
+++ src/errors/handler.ts
@@ -0,0 +1,9 @@
+import { error } from 'console';
+import { ErrorRequestHandler } from 'express';
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  console.error(error);
+  return response.status(500).json({ message: 'Internal server error' })
+};
+
+export default errorHandler;
